2c292cae08c2a52f10eb40d7d6c6af89bd079c9f
Philipp Haack <philippHaackCode@gmail.com>
2021-11-25 19:15:10 +0100

    first_bad_version fixed - (code w/ solu) - leetcode

--- leetcode/first_bad_version.cpp
+++ leetcode/first_bad_version.cpp
@@ -4,25 +4,16 @@ bool isBadVersion(int n);
 
 int first_bad_version(int n)
 {
-    if(n==1) return 1;
-    std::vector<bool> values(n);
-    std::vector<bool> ready(n);
-
-    int l = 0, r = n;
-    while(l<=r)
-    {
-        int mid = l + (r - l) / 2;
-        //for minimizing API calls save in memorizing ds
-        bool isBad_mid = ready[mid] ? values[mid] :  isBadVersion(mid);
-        bool isBad_after = ready[mid + 1] ? values[mid+1] : isBadVersion(mid+1);
-        ready[mid] = true; values[mid] = isBad_mid;
-        ready[mid+1] = true; values[mid+1] = isBad_after;
-
-        if(!isBad_mid && isBad_after) return mid+1;
-        else if(isBad_mid) r = mid-1;
-        else if(!isBad_mid) l = mid+1;
-    }
-    return -1;
+    int start = 1, end = n;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid)) {
+                end = mid ;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
 }
 
 
